6d16d831cef6bcb3edcb4b41605edaaab93f2d10
Mohamed Tony <mohamed.tony.8173@gmail.com>
2025-12-20 09:06:25 +0200

    app tasks with models task,epic and sprint

---
+++ tasks/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from tasks.models import Task,Epic,Sprint
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "status",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status",)
+    actions = ["mark_archived"]
+
+    def mark_archived(self, request, queryset):
+        queryset.update(status="ARCHIVED")
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.has_perm("tasks.change_task"):
+            return True
+        return False
+
+    def has_add_permission(self, request):
+        if request.user.has_perm("tasks.add_task"):
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.has_perm("tasks.delete_task"):
+            return True
+        return False
+
+    mark_archived.short_description = " mark status as archived"
+
+
+
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Epic)
+admin.site.register(Sprint)
+
